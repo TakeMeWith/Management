e2f51bb8f8b8413004aa57b2740d6323a6339489
TakeMeWith <swh45914255@gmail.com>
2021-07-08 16:59:29 +0900

    asdf

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './App.css';
 import Customer from './components/Customer';
 import './App.css'
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/api/hello', (req,res) => {
-    res.send({message : 'Yahaha'});
+    res.send({message : 'Yahahaaaa'});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
