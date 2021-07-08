100b97b59d91fb00cff24dd9eabaa061c20e9f3c
TakeMeWith <swh45914255@gmail.com>
2021-07-08 16:22:01 +0900

    Add express nodejs server module

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/api/hello', (req,res) => {
-    res.send({message : 'Please!'});
+    res.send({message : 'Yahaha'});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
